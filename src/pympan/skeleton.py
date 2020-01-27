3093226ccb4addf288a6ab25d8a1333edbe3c59a
David Parr <davidrgparr@gmail.com>
2020-01-27 13:45:13 +0000

    create function to compare MPAN with the function to calculate the check digit

--- src/pympan/skeleton.py
+++ src/pympan/skeleton.py
@@ -25,9 +25,7 @@ __author__ = "David Parr"
 __copyright__ = "David Parr"
 __license__ = "mit"
 
-_logger = logging.getLogger(__name__)
-
-def check_digit(mpan):
+def calculate_check_digit(mpan_unique: str) -> int:
     """Check MPAN digit.
 
     The final digit in the MPAN is the check digit, and validates the previous 12 (the core) using a modulus 11 test. The check digit is calculated thus:
@@ -41,91 +39,32 @@ def check_digit(mpan):
     Args:
       mpan -- The first 12 digits of the MPAN number, excluding the check digit.
     """
-    return sum(prime * int(digit) for prime, digit in \
-            zip([3, 5, 7, 13, 17, 19, 23, 29, 31, 37, 41, 43], mpan)) % 11 % 10
+    check_digit = sum(prime * int(digit) for prime, digit in \
+            zip([3, 5, 7, 13, 17, 19, 23, 29, 31, 37, 41, 43], mpan_unique)) % 11 % 10
 
-def fib(n):
-    """Fibonacci example function
+    return check_digit
 
-    Args:
-      n (int): integer
-
-    Returns:
-      int: n-th Fibonacci number
+def compare_check_digit(mpan_core: str) -> bool:
     """
-    assert n > 0
-    a, b = 1, 1
-    for i in range(n-1):
-        a, b = b, a+b
-    return a
-
-
-def parse_args(args):
-    """Parse command line parameters
-
-    Args:
-      args ([str]): command line parameters as list of strings
-
-    Returns:
-      :obj:`argparse.Namespace`: command line parameters namespace
+    compare_check_digit [summary]
+    
+    :param mpan_core: [description]
+    :type mpan_core: str
+    :return: [description]
+    :rtype: bool
     """
-    parser = argparse.ArgumentParser(
-        description="Just a Fibonacci demonstration")
-    parser.add_argument(
-        "--version",
-        action="version",
-        version="pympan {ver}".format(ver=__version__))
-    parser.add_argument(
-        dest="n",
-        help="n-th Fibonacci number",
-        type=int,
-        metavar="INT")
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        dest="loglevel",
-        help="set loglevel to INFO",
-        action="store_const",
-        const=logging.INFO)
-    parser.add_argument(
-        "-vv",
-        "--very-verbose",
-        dest="loglevel",
-        help="set loglevel to DEBUG",
-        action="store_const",
-        const=logging.DEBUG)
-    return parser.parse_args(args)
 
+    n = len(mpan_core)
 
-def setup_logging(loglevel):
-    """Setup basic logging
+    if n != 13:
+      print("`mpan_core` is not 13")
 
-    Args:
-      loglevel (int): minimum loglevel for emitting messages
-    """
-    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
-    logging.basicConfig(level=loglevel, stream=sys.stdout,
-                        format=logformat, datefmt="%Y-%m-%d %H:%M:%S")
-
-
-def main(args):
-    """Main entry point allowing external calls
-
-    Args:
-      args ([str]): command line parameter list
-    """
-    args = parse_args(args)
-    setup_logging(args.loglevel)
-    _logger.debug("Starting crazy calculations...")
-    print("The {}-th Fibonacci number is {}".format(args.n, fib(args.n)))
-    _logger.info("Script ends here")
-
-
-def run():
-    """Entry point for console_scripts
-    """
-    main(sys.argv[1:])
-
-
-if __name__ == "__main__":
-    run()
+    else:
+      check_digit = calculate_check_digit(mpan_core[0:12])
+      if check_digit == int(mpan_core[12]):
+        print("check digit checks out")
+        check_response = True
+      else:
+        print("check your digits")
+        check_response = False
+    return check_response
--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -1,21 +1,48 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from pympan.skeleton import check_digit
+from pympan.skeleton import calculate_check_digit, compare_check_digit
 
 __author__ = "David Parr"
 __copyright__ = "David Parr"
 __license__ = "mit"
 
-def test_check_digit():
-    assert check_digit("210004056876") == 5
-    assert check_digit("219998966488") == 2
-    assert check_digit("219999121347") == 4
-    assert check_digit("219999131116") == 2
-    assert check_digit("219999138049") == 5
-    assert check_digit("219999141655") == 7
-    assert check_digit("219999142127") == 3
-    assert check_digit("219999150607") == 7
-    assert check_digit("219999153889") == 5
+@pytest.fixture(name="full_mpans")
+def fixture_full_mpans(request):
+    """
+    2100040568765	K06D00347
+    2199989664882	208280757
+    2199991213474	211047941
+    2199991311162	211048796
+    2199991380495	210201010
+    2199991416557	E12Z040308
+    2199991421273	E12Z092282
+    2199991506077	211048777
+    2199991538895	211049032
+    """
+    return "2110479412199991213474"
+
+def test_calculate_check_digit():
+    assert calculate_check_digit("210004056876") == 5
+    assert calculate_check_digit("219998966488") == 2
+    assert calculate_check_digit("219999121347") == 4
+    assert calculate_check_digit("219999131116") == 2
+    assert calculate_check_digit("219999138049") == 5
+    assert calculate_check_digit("219999141655") == 7
+    assert calculate_check_digit("219999142127") == 3
+    assert calculate_check_digit("219999150607") == 7
+    assert calculate_check_digit("219999153889") == 5
+
+def test_compare_check_digit():
+    assert compare_check_digit("2100040568765") == True
+    assert compare_check_digit("2199989664882") == True
+    assert compare_check_digit("2199991213474") == True
+    assert compare_check_digit("2199991311162") == True
+    assert compare_check_digit("2199991380495") == True
+    assert compare_check_digit("2199991416557") == True
+    assert compare_check_digit("2199991421273") == True
+    assert compare_check_digit("2199991506077") == True
+    assert compare_check_digit("2199991538895") == True
+    assert compare_check_digit("2199991538894") == False
 
     
